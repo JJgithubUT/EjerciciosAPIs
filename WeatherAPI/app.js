424d7d8c6cc33210e0916fbb2c0f6571a7d930df
JuanJavier <juanjcbreton@gmail.com>
2024-11-06 11:59:12 -0600

    5.0 Avance en el weather 6-11-24

--- WeatherAPI/app.js
+++ WeatherAPI/app.js
@@ -1,17 +1,22 @@
-const URL = 'https://api.openweathermap.org/data/2.5/weather?';
+const URL_base = 'https://api.openweathermap.org/data/2.5/weather?';
 const API_KEK = '860c0bea7c5bf2e4b7920c986434d4eb';
 const API_KEK_2 = '5feb021e54bb530cb784904e228e5013';
+const cuerpo = document.querySelector('body');
+
+let data;
 
 async function getClime(lat, lon) {
-    const result = await fetch(URL_BASE + `lat=${lat}&lon=${lon}&appid=${API_KEK}&units=metric`);
-    const data = await result.json();
+    const result = await fetch(URL_base + `lat=${lat}&lon=${lon}&appid=${API_KEK_2}&units=metric`);
+    data = await result.json();
     console.log(data);
 }
 
 navigator.geolocation.getCurrentPosition((pos) => {
-    console.log(pos.coords.latitude);
-    console.log(pos.coords.longitude);
-    getClime(pos.coords.latitude, pos.coords.longitude);
+    const lat = pos.coords.latitude
+    const lon = pos.coords.longitude
+    console.log(lat);
+    console.log(lon);
+    getClime(lat, lon);
+    cuerpo.style.backgroundColor = 'aqua';
 });
 
-// lat={lat}&lon={lon}&appid={API key}
