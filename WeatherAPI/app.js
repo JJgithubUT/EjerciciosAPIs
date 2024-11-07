91d40dd3925735ef6fb7c92a6bf3b1bcb5727878
JuanJavier <juanjcbreton@gmail.com>
2024-11-07 11:10:18 -0600

    5.2 Actualización de hoy: Buscador, casi sale termómetro 7-11-24

--- WeatherAPI/app.js
+++ WeatherAPI/app.js
@@ -1,7 +1,12 @@
+//           おれの住所の情報
 const URL_base = 'https://api.openweathermap.org/data/2.5/weather?';
 const API_KEK = '860c0bea7c5bf2e4b7920c986434d4eb';
 const API_KEK_2 = '5feb021e54bb530cb784904e228e5013';
-const numeroTermometro = document.querySelector('.spanNumeroTermometro');
+const numeroTermometroShow = document.querySelector('.spanNumeroTermometro');
+const barra1 = document.querySelector('.subbarra1');
+const barra2 = document.querySelector('.subbarra2');
+const botonBuscarCity = document.getElementById('botonBuscarCity');
+const inputBuscarCity = document.getElementById('inputBuscarCity');
 
 let data;
 
@@ -12,7 +17,22 @@ async function getClime(lat, lon) {
     return data;
 }
 
+async function getClimeByCity(city) {
+    const result = await fetch(URL_base + `q=${city}&appid=${API_KEK_2}&units=metric`);
+    data = await result.json();
+    console.log(data);
+    return data;
+}
 
+function cambiarBarra(temperatura) {
+    if (temperatura > 0) {
+        barra1.
+    } else if (temperatura < 0) {
+
+    } else if ( temperatura == 0) {
+
+    }
+}
 
 navigator.geolocation.getCurrentPosition((pos) => {
     const lat = pos.coords.latitude
@@ -22,3 +42,8 @@ navigator.geolocation.getCurrentPosition((pos) => {
     getClime(lat, lon);
 });
 
+botonBuscarCity.addEventListener('click', () => {
+    if (inputBuscarCity.value !== null) {
+        getClimeByCity(inputBuscarCity.value);
+    }
+});
