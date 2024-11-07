ce0ff56edcd8b5a01230e385b8b37fcedb9e0a17
JuanJavier <juanjcbreton@gmail.com>
2024-11-06 20:02:59 -0600

    5.1 Actualización estilos para weather 6-11-24

--- WeatherAPI/app.js
+++ WeatherAPI/app.js
@@ -1,7 +1,7 @@
 const URL_base = 'https://api.openweathermap.org/data/2.5/weather?';
 const API_KEK = '860c0bea7c5bf2e4b7920c986434d4eb';
 const API_KEK_2 = '5feb021e54bb530cb784904e228e5013';
-const cuerpo = document.querySelector('body');
+const numeroTermometro = document.querySelector('.spanNumeroTermometro');
 
 let data;
 
@@ -9,7 +9,10 @@ async function getClime(lat, lon) {
     const result = await fetch(URL_base + `lat=${lat}&lon=${lon}&appid=${API_KEK_2}&units=metric`);
     data = await result.json();
     console.log(data);
+    return data;
 }
+
+
 
 navigator.geolocation.getCurrentPosition((pos) => {
     const lat = pos.coords.latitude
@@ -17,6 +20,5 @@ navigator.geolocation.getCurrentPosition((pos) => {
     console.log(lat);
     console.log(lon);
     getClime(lat, lon);
-    cuerpo.style.backgroundColor = 'aqua';
 });
 
