fa953b5ada9913bebce0c4d28c8bf034a0103304
JuanJavier <juanjcbreton@gmail.com>
2024-11-11 15:35:49 -0600

    5.3 Actualización del viernes(8-11-24), hoy es lunes(11-11-24) 11-11-24

--- WeatherAPI/app.js
+++ WeatherAPI/app.js
@@ -2,11 +2,16 @@
 const URL_base = 'https://api.openweathermap.org/data/2.5/weather?';
 const API_KEK = '860c0bea7c5bf2e4b7920c986434d4eb';
 const API_KEK_2 = '5feb021e54bb530cb784904e228e5013';
-const numeroTermometroShow = document.querySelector('.spanNumeroTermometro');
+const numeroTermometroShow = document.querySelector('#spanNumeroTermometro');
 const barra1 = document.querySelector('.subbarra1');
 const barra2 = document.querySelector('.subbarra2');
 const botonBuscarCity = document.getElementById('botonBuscarCity');
 const inputBuscarCity = document.getElementById('inputBuscarCity');
+
+const spanNombre = document.getElementById('spanNombre');
+const spanPais = document.getElementById('spanPais');
+const spanClima = document.getElementById('spanClima');
+const spanHora = document.getElementById('spanHora');
 
 let data;
 
@@ -14,6 +19,23 @@ async function getClime(lat, lon) {
     const result = await fetch(URL_base + `lat=${lat}&lon=${lon}&appid=${API_KEK_2}&units=metric`);
     data = await result.json();
     console.log(data);
+
+    const temperatura = data.main.temp;
+    cambiarBarra(temperatura);
+    numeroTermometroShow.innerText = `${temperatura}°C`;
+    const nombreCiudad = data.name;
+    const pais = data.sys.country;
+    const clima = data.weather[0].main;
+    spanNombre.innerText = nombreCiudad;
+    spanPais.innerText = pais;
+    spanClima.innerText = clima;
+
+    const timestamp = data.dt;
+    const timezoneOffset = data.timezone;
+    const date = new Date((timestamp + timezoneOffset) * 1000);
+    spanHora.innerText = date.toUTCString();
+    inputBuscarCity.value = nombreCiudad;
+
     return data;
 }
 
@@ -21,29 +43,52 @@ async function getClimeByCity(city) {
     const result = await fetch(URL_base + `q=${city}&appid=${API_KEK_2}&units=metric`);
     data = await result.json();
     console.log(data);
+    
+    const temperatura = data.main.temp;
+    cambiarBarra(temperatura);
+    numeroTermometroShow.innerText = `${temperatura}°C`;
+    const nombreCiudad = data.name;
+    const pais = data.sys.country;
+    const clima = data.weather[0].main;
+    spanNombre.innerText = nombreCiudad;
+    spanPais.innerText = pais;
+    spanClima.innerText = clima;
+
+    const timestamp = data.dt;
+    const timezoneOffset = data.timezone;
+    const date = new Date((timestamp + timezoneOffset) * 1000);
+    spanHora.innerText = date.toUTCString();
+
     return data;
 }
 
 function cambiarBarra(temperatura) {
     if (temperatura > 0) {
-        barra1.
+        barra1.style.height = `0%`;
+        barra2.style.height = `0%`;
+        barra1.style.height = `${ temperatura / 50 * 100 }%`;
     } else if (temperatura < 0) {
-
-    } else if ( temperatura == 0) {
-
+        barra1.style.height = `0%`;
+        barra2.style.height = `0%`;
+        barra2.style.height = `${ 0 - temperatura / 50 * 100 }%`;
+        console.log(temperatura);
+    } else if (temperatura === 0) {
+        barra1.style.height = `0%`;
+        barra2.style.height = `0%`;
     }
 }
 
 navigator.geolocation.getCurrentPosition((pos) => {
-    const lat = pos.coords.latitude
-    const lon = pos.coords.longitude
+    const lat = pos.coords.latitude;
+    const lon = pos.coords.longitude;
     console.log(lat);
     console.log(lon);
     getClime(lat, lon);
 });
 
 botonBuscarCity.addEventListener('click', () => {
-    if (inputBuscarCity.value !== null) {
+    if (inputBuscarCity.value !== "") {
         getClimeByCity(inputBuscarCity.value);
+        
     }
 });
